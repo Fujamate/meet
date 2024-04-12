7d178da6a5227bde36a93f423f8c5407ac4275f9
Kevin Hölscher <kevin-hoelscher@gmx.de>
2024-04-12 12:37:53 +0200

    4.9

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import EventList from "./components/EventList";
 import NumberOfEvents from "./components/NumberOfEvents";
 import { useEffect, useState } from "react";
 import { extractLocations, getEvents } from "./api";
-import { InfoAlert, ErrorAlert } from "./components/Alert";
+import { InfoAlert, ErrorAlert, WarningAlert } from "./components/Alert";
 
 import "./App.css";
 
@@ -14,6 +14,16 @@ const App = () => {
   const [currentCity, setCurrentCity] = useState("See all cities");
   const [infoAlert, setInfoAlert] = useState("");
   const [errorAlert, setErrorAlert] = useState("");
+  const [warningAlert, setWarningError] = useState("");
+
+  useEffect(() => {
+    if (navigator.onLine) {
+      setWarningError("");
+    } else {
+      setWarningError("You are offline, events are loaded from cache!");
+    }
+    fetchData();
+  }, [currentCity, currentNOE]);
 
   useEffect(() => {
     fetchData();
@@ -34,6 +44,7 @@ const App = () => {
       <div className="alerts-container">
         {infoAlert.length ? <InfoAlert text={infoAlert} /> : null}
         {errorAlert.length ? <ErrorAlert text={errorAlert} /> : null}
+        {warningAlert.length ? <WarningAlert text={warningAlert} /> : null}
       </div>
       <CitySearch
         allLocations={allLocations}
